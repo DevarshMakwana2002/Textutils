f3d2587c735a19ec0335db10506c19d3728a11c0
Devarsh Makwana <devarshppsv@gmail.com>
2023-01-20 13:16:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import NaveBar from './components/NaveBar';
+function App() {
+  return (
+    <>
+      <NaveBar title="Textutils" AboutText="About"/>
+    </>
+  );
+}
+
+export default App;
